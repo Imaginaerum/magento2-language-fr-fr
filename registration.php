eb1df3e838692d0554606238b88841aa197837ee
Sylvain Kocet <sylvain@monacoweb-mc.net>
2016-01-13 18:39:46 +0100

    v100.0.0

---
+++ registration.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Magento2 Language fr_fr
+ * Github: https://github.com/Imaginaerum/magento2-language-fr-fr
+ * 
+ * @author Sylvain Kocet <pyksel@gmail.com>
+ * @copyright Copyright © 2016 Imaginaerum
+ * @version 100.0.0
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::LANGUAGE, 'imaginaerum_fr_fr', __DIR__
+);
