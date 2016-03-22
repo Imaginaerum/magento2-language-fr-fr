06165ab35967126e47e16eab8783c90130bab163
Sylvain Kocet <sylvain@monacoweb-mc.net>
2016-03-22 17:16:19 +0100

    v100.4.1

--- registration.php
+++ registration.php
@@ -9,7 +9,7 @@
  * 
  * @author Sylvain Kocet <pyksel@gmail.com>
  * @copyright Copyright © 2016 Imaginaerum
- * @version 100.4.0
+ * @version 100.4.1
  */
 \Magento\Framework\Component\ComponentRegistrar::register(
         \Magento\Framework\Component\ComponentRegistrar::LANGUAGE, 'imaginaerum_fr_fr', __DIR__
