625229fb92806c9a30e5edeb94e6b9c82f0a9584
Sylvain Kocet <pyksel@gmail.com>
2016-05-04 11:22:10 +0200

    master

---
+++ registration.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Magento2 French Language Pack (fr_FR)
+ * 
+ * Website: http://imaginaerum.github.io/magento2-language-fr-fr
+ * Github: https://github.com/Imaginaerum/magento2-language-fr-fr
+ * Packagist: https://packagist.org/packages/imaginaerum/magento2-language-fr-fr
+ * 
+ * @author Sylvain Kocet <pyksel@gmail.com>
+ * @copyright Copyright © 2016 Imaginaerum
+ * @version 100.6.8
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::LANGUAGE, 'imaginaerum_fr_fr', __DIR__
+);
