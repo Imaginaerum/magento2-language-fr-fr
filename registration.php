631554fb1ff7e19138f6b859ba837457f4538f1c
Imaginaerum <pyksel@gmail.com>
2016-07-26 08:53:18 +0200

    Remove whitespace

--- registration.php
+++ registration.php
@@ -2,11 +2,11 @@
 
 /**
  * Magento2 French Language Pack (fr_FR)
- * 
+ *
  * Website: http://imaginaerum.github.io/magento2-language-fr-fr
  * Github: https://github.com/Imaginaerum/magento2-language-fr-fr
  * Packagist: https://packagist.org/packages/imaginaerum/magento2-language-fr-fr
- * 
+ *
  * @author Sylvain Kocet <pyksel@gmail.com>
  * @copyright Copyright © 2016 Imaginaerum
  * @version 100.6.18
